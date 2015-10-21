4dda5e3cf096a20f4fb4c3e43204582c449f896b
Proskurina <yproskurina@yahoo.com>
2015-10-21 20:54:17 +0100

    moved bower_components to public

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
