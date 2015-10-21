932d84d93e7c6ae6db88498934cb01ba98dbb4e8
Proskurina <yproskurina@yahoo.com>
2015-10-21 20:51:39 +0100

    changed port

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ app.get('/', function (req, res) {
 });
 
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+app.listen(process.env.PORT || 3000, function() {
+  console.log("listening on 3000");
 });
 
