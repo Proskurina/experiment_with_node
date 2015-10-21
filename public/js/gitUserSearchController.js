4c7f220727c505e732e77b94bdd01c3a6cd95cbe
Proskurina <yproskurina@yahoo.com>
2015-10-21 19:32:22 +0100

    Gitprofile search

---
+++ public/js/gitUserSearchController.js
@@ -0,0 +1,13 @@
+githubUserSearch.controller('GitUserSearchController', ['Search', function(Search) {
+
+  var self = this;
+
+  self.doSearch = function() {
+    if (self.searchTerm) {
+      Search.query(self.searchTerm)
+        .then(function(response) {
+          self.searchResult = response.data;
+        });
+    };
+  };
+}]);
